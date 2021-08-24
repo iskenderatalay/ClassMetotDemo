e933d89e1c507ea85482adb039a6d82534ca0309
iskender <iskenderatalay@hotmail.com>
2021-08-25 02:22:36 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi"+ musteri.Ad +" "+ musteri.Soyad);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi" + musteri.Ad + " "+musteri.Soyad);
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Ad:" + musteri.Ad);
+                Console.WriteLine("Soyad:" + musteri.Soyad);
+                Console.WriteLine("---");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "XZ";
+            musteri1.Soyad = "Corp";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Zee";
+            musteri2.Soyad = "body";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+           
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriSil(musteri2);
+            musteriManager.MusteriListele(musteriler);
+
+            Console.ReadLine();
+        }
+    }
+}
